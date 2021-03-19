a3f7e22691b27d55339dc326ea9e2a7c55b5717d
gitgeoman <gitgeoman@gmail.com>
2021-03-19 11:39:09 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,2 +1,14 @@
 //16:50
-const firebaseConfig = {};
+//2:56
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyBB7kgRX93zywVXljhBIWbGeXWwl3Su-i0",
+  authDomain: "facebook-clone-437ff.firebaseapp.com",
+  projectId: "facebook-clone-437ff",
+  storageBucket: "facebook-clone-437ff.appspot.com",
+  messagingSenderId: "242276638463",
+  appId: "1:242276638463:web:1e40ed9447ae3bb2f2f6ca",
+  measurementId: "G-EWXMEYH3GM",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
